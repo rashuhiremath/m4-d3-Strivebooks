5432f0a205df1c47bd2a861a3b0c8f375c3f670e
rashuhiremath <rashmi.hiremath94@gmail.com>
2021-09-29 15:13:51 +0200

    updated

--- strive-books/src/App.js
+++ strive-books/src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import MyBadge from './Components/MyBadge';
+import WarningSign from './Components/WarningSign';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <WarningSign text="your books available"/>
+        <MyBadge text = "whatever" color ="primary" />
+        
       </header>
     </div>
   );
